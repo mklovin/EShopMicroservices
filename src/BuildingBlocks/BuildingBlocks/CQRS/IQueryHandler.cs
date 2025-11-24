bddd9410a2d1e173deb6c302aca2999e09d3fa25
Ali <khraim13@gmail.com>
2025-11-23 21:08:50 -0500

    feat(week-one) Build and configure the product side of the app

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+namespace BuildingBlocks.CQRS;
+
+
+public interface IQueryHandler <in TQuery> : IRequestHandler<TQuery, Unit>
+    where TQuery : IQuery<Unit>
+{
+}
+
+public interface IQueryHandler<in TQuery, TRespone>: IRequestHandler<TQuery, TRespone>
+    where TQuery : IQuery<TRespone>
+    where TRespone : notnull
+{
+}
---
+++ src/Services/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
